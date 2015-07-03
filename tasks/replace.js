e5f19dbac46337d87107bfd27628ce1dfa72a66b
immadimtiaz10p <immad.imtiaz@tenpearls.com>
2015-07-03 15:30:24 +0500

    Added initial app structure.

---
+++ tasks/replace.js
@@ -0,0 +1,26 @@
+module.exports = {
+  scripts: {
+    src: ['.tmp/concat/scripts/*.js'],
+    overwrite: true,
+    replacements: [{
+      from: /src\/[a-zA-Z0-9]*\//g,
+      to: 'views/'
+    },
+      {
+        from: "angular.module('app', [",
+        to: "angular.module('app', ['templates-htmlmin',"
+      }]
+  },
+  styles: {
+    src: ['dist/styles/main.css'],
+    overwrite: true,
+    replacements: [{
+      from: '../../fonts',
+      to: '../fonts'
+    },
+      {
+        from: '../../images',
+        to: '../images'
+      }]
+  }
+};
