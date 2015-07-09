8091d6037ebdd374b63bffa915770340f80a9537
Immad Imtiaz <immadimtiaz@gmail.com>
2015-07-09 07:00:47 +0500

    Progress

--- app/src/tradebook/addtransaction.controller.js
+++ app/src/tradebook/addtransaction.controller.js
@@ -17,69 +17,19 @@
 		var vm = this;
 
 		vm.testFunction = testFunction;
-        //=======================================================
-  //Email Contacts
+    //=======================================================
+  //Single Item Select
   //=======================================================
-  $scope.emails; 
+  $scope.single = null;
   
-
-  var REGEX_EMAIL = '([a-z0-9!#$%&\'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&\'*+/=?^_`{|}~-]+)*@' +
-                  '(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?)';
-  $scope.emailsConfig = {
-    persist: false,
-    maxItems: null,
-    valueField: 'email',
-    labelField: 'name',
-    options: [{email: 'brian@thirdroute.com', name: 'Brian Reavis'},
-        {email: 'nikola@tesla.com', name: 'Nikola Tesla'},
-        {email: 'someone@gmail.com'}],
-    searchField: ['name', 'email'],
-    render: {
-        item: function(item, escape) {
-            return '<div>' +
-                (item.name ? '<span class="name">' + escape(item.name) + '</span>' : '') +
-                (item.email ? '<span class="email">' + escape(item.email) + '</span>' : '') +
-            '</div>';
-        },
-        option: function(item, escape) {
-            var label = item.name || item.email;
-            var caption = item.name ? item.email : null;
-            return '<div>' +
-                '<span class="label">' + escape(label) + '</span>' +
-                (caption ? '<span class="caption">' + escape(caption) + '</span>' : '') +
-            '</div>';
-        }
-    },
-    createFilter: function(input) {
-        var match, regex;
-
-        // email@address.com
-        regex = new RegExp('^' + REGEX_EMAIL + '$', 'i');
-        match = input.match(regex);
-        if (match) return !this.options.hasOwnProperty(match[0]);
-
-        // name <email@address.com>
-        regex = new RegExp('^([^<]*)\<' + REGEX_EMAIL + '\>$', 'i');
-        match = input.match(regex);
-        if (match) return !this.options.hasOwnProperty(match[2]);
-
-        return false;
-    },
-    create: function(input) {
-        if ((new RegExp('^' + REGEX_EMAIL + '$', 'i')).test(input)) {
-            return {email: input};
-        }
-        var match = input.match(new RegExp('^([^<]*)\<' + REGEX_EMAIL + '\>$', 'i'));
-        if (match) {
-            return {
-                email : match[2],
-                name  : $.trim(match[1])
-            };
-        }
-        alert('Invalid email address.');
-        return false;
-    }
+  $scope.singleConfig = {
+    options: [{value: 1, text: 'Chuck Testa', email:'chucktesla@gmail,com'}, {value: 2, text:'Nikola Tesla',email:'nuckTesla@gmail.com'}],
+    create: true,
+    sortField: 'text',
+    maxItems: 1,
   }
+  
+ 
 
     /////////////////////
 
