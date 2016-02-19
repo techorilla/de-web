c19be5f68f37b2678b9537517c4097895deaa971
immadimtiaz10p <immad.imtiaz@tenpearls.com>
2016-02-19 20:16:46 +0500

    ErrorInterceptor

--- app/src/core/core.module.js
+++ app/src/core/core.module.js
@@ -66,14 +66,18 @@
         });
     });
 
-    angular.module('app.core').config(function($httpProvider) {
+    angular.module('app.core').config(function($httpProvider,httpStatus) {
+
         $httpProvider.interceptors.push(function($q, $injector) {
             return {
                 'responseError': function(rejection){
                     var defer = $q.defer();
-                    console.log(rejection);
-                    if(rejection.status == 401){
-                        $injector.get('$state').transitionTo('login');;
+                    if(rejection.status == httpStatus.INTERNAL_SERVER_ERROR){
+                        $injector.get('navigation').internalServerError(rejection.data,rejection.statusText);
+                    }
+
+                    if(rejection.status == httpStatus.UNAUTHORIZED){
+                        $injector.get('$state').transitionTo('login');
                     }
                     defer.reject(rejection);
                     return defer.promise;
